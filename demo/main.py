200d77c35a5e8397d5b021cfd704ba515dfd7cca
Johnny Chen <johnchen5372@gmail.com>
2019-09-06 13:37:08 +0800

    fix Warnning

--- demo/main.py
+++ demo/main.py
@@ -7,6 +7,12 @@ from datetime import date
 
 import pymysql
 from sqlalchemy import create_engine
+# 指定过滤告警的类别为 pymysql.Warning类
+from warnings import filterwarnings
+# 动作为"error",该动作可以抛错，则可以用try ... except 捕获
+#filterwarnings("error",category=pymysql.Warning)
+# 动作为"ignore", 表示忽略
+filterwarnings("ignore",category=pymysql.Warning)
 
 import pandas as pd
 # 自定义功能module
@@ -40,37 +46,45 @@ if __name__ == "__main__":
     passwd = str(mysql_info.get('passwd', None))
     database = str(mysql_info.get('database', None))
 
-    engine = create_engine("mysql+pymysql://%s:%s@%s:3306/%s?charset=utf8" % (user, passwd, host, database))
+    engine = create_engine("mysql+pymysql://%s:%s@%s:3306/%s?charset=utf8" % \
+            (user, passwd, host, database))
     #basic cal data
     for exchange in exchangelist:
         try:
-            df = pd.read_excel("backup/cal_%s.xls" % str(exchange))
+            df = pd.read_excel("backup/%s_%scal.xls" % (exchange, y_m_d))
             # print(df)
-            df.to_sql(name=str('cal_'+ exchange),con=engine,if_exists='replace',index=False,index_label=False)
+            df.to_sql(name=str(exchange.lower() +'_'+ y_m_d +'cal'),con=engine, \
+                    if_exists='replace',index=False,index_label=False)
         except Exception as err:
             print(err)
     
     #basic stocklist data
     for exchange in exchangelist:
         try:
-            df = pd.read_excel("backup/stocklist_%s.xls" % str(exchange))
+            df = pd.read_excel("backup/%s_%sstocklist.xls" % (exchange, y_m_d))
             # print(df)
             for row in df.itertuples():
                 #print(row.ts_code)
                 stocklist.append(str(row.ts_code))
             # print(stocklist)
-            df.to_sql(name=str('stocklist_'+ exchange),con=engine,if_exists='replace',index=False,index_label=False)
+            df.to_sql(name=str(exchange.lower() +'_'+ y_m_d +'stocklist'),con=engine, \
+                    if_exists='replace',index=False,index_label=False)
         except Exception as err:
             print(err)
 
     # daily data
+    #total = len(stock_pool)
+    total = len(stocklist)
+    msg = ''
     # for i in range(len(stock_pool)):
     for i in range(len(stocklist)):
         try:
             # df = pd.read_excel("backup/daily_%s.xls" % str(stock_pool[i]))
-            df = pd.read_excel("backup/daily_%s.xls" % str(stocklist[i]))
+            df = pd.read_excel("backup/daily_%s.xls" % str(stocklist[i])[:-3])
             # print(df)
-            # df.to_sql(name=str('daily_'+ stock_pool[i]),con=engine,if_exists='replace',index=False,index_label=False)
-            df.to_sql(name=str('daily_'+ stocklist[i]),con=engine,if_exists='replace',index=False,index_label=False)
+            # df.to_sql(name=str('daily_'+ stock_pool[i][:-3]),con=engine,if_exists='replace',index=False,index_label=False)
+            df.to_sql(name=str('daily_'+ stocklist[i][:-3]),con=engine,if_exists='replace',index=False,index_label=False)
+            # print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' + str(stock_pool[i]))
+            print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' + str(stocklist[i]))
         except Exception as err:
             print(err) 
