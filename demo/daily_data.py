590e8e47c7a002ad8795d6f403dad75f9060ed94
John.Chen <johnchen5372@gmail.com>
2019-10-24 21:08:54 +0800

    update daily_data.py

--- demo/daily_data.py
+++ demo/daily_data.py
@@ -15,9 +15,11 @@ import tushare as ts
 自定义的module
 '''
 import cfg
-import basic_data
 
-def main_stockpool(path):
+'''
+get daily data in stock pool
+'''
+def main(path):
     config = os.path.join(path, '../ts_config.ini')
 #    print(config)
     stock_info = cfg.read_ini(config, 'stock')
@@ -43,36 +45,10 @@ def main_stockpool(path):
         except Exception as err:
             print(err)
 
-
-def main(path):
-    config = os.path.join(path, '../ts_config.ini')
-#    print(config)
-    ts_info = cfg.read_ini(config, 'tushare')
-    ts_token = str(ts_info.get('cpp_token', None))
-#    print(ts_token)
-    pro = ts.pro_api(ts_token)
-#    print(pro)
-    stocklist = basic_data.get_stocklist(ts_token)
-    total = len(stocklist)
-    msg = ''
-    for i in range(len(stocklist)):
-        try:
-            df = pro.daily(ts_code=stocklist[i], start_date='19920101',
-                    end_date=date.today().strftime('%Y%m%d'))
-            df.to_excel("backup/daily_%s.xls" % str(stocklist[i])[:-3])
-            print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' +
-                    str(stocklist[i]) + ' has been backup to excel!')
-            msg += str(df) + '\n'
-#            print(msg)
-#            cfg.send_mail(config, msg)
-        except Exception as err:
-            print(err)
-
 '''
 获取stock daily data, backup to excel sheet
 '''
 if __name__ == "__main__":
     cur_path = sys.argv[0].rstrip('/daily_data.py')
 #    print(cur_path)
-    main_stockpool(cur_path)
     main(cur_path)
