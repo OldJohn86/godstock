2552ea230ad88897068c4699711a427ac4b26086
Johnny Chen <johnchen5372@gmail.com>
2019-10-24 14:39:35 +0800

    update daily_data.py

--- demo/daily_data.py
+++ demo/daily_data.py
@@ -1,54 +1,80 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
-获取stock daily data, backup to excel sheet
-'''
+
+__author__ = 'Jonathan Chen'
+__date__   = '2019-09-09'
 
 import os
 import sys
 import time
 import datetime
 from datetime import date
-
 import tushare as ts
 
-# 自定的module
+'''
+自定义的module
+'''
 import cfg
 import basic_data
 
-if __name__ == "__main__":
-    y_m_d = date.today().strftime('%Y%m%d')
-    # print(y_m_d)
-
-    current_path = sys.argv[0].rstrip('/daily_data.py')
-    # print(current_path)
-    config = os.path.join(current_path, '../ts_config.ini')
-    # print(config)
-    ts_info = cfg.read_ini(config, 'tushare')
-    ts_token = str(ts_info.get('cpp_token', None))
-    # print(ts_token)
-    pro = ts.pro_api(ts_token)
-    # print(pro)
+def main_stockpool(path):
+    config = os.path.join(path, '../ts_config.ini')
+#    print(config)
     stock_info = cfg.read_ini(config, 'stock')
     stock_pool = str(stock_info.get('stock_pool', None)).split()
-    # print(stock_pool)
-
-    stocklist = basic_data.get_stocklist(ts_token)
-    #total = len(stock_pool)
-    total = len(stocklist)
+#    print(stock_pool)
+    ts_info = cfg.read_ini(config, 'tushare')
+    ts_token = str(ts_info.get('cpp_token', None))
+#    print(ts_token)
+    pro = ts.pro_api(ts_token)
+#    print(pro)
+    total = len(stock_pool)
     msg = ''
-    #for i in range(len(stock_pool)):
-    for i in range(len(stocklist)):
+    for i in range(len(stock_pool)):
         try:
-            # df = pro.daily(ts_code=stock_pool[i], start_date='19920101', end_date=date.today().strftime('%Y%m%d'))
-            df = pro.daily(ts_code=stocklist[i], start_date='19920101', end_date=date.today().strftime('%Y%m%d'))
-            # df = pro.daily(ts_code=stock_pool[i], start_date='20190801', end_date='20190831')
-            # df.to_excel("backup/daily_%s.xls" % str(stock_pool[i]))
-            df.to_excel("backup/daily_%s.xls" % str(stocklist[i])[:-3])
-            # print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' + str(stock_pool[i]))
-            print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' + str(stocklist[i]))
-
+            df = pro.daily(ts_code=stock_pool[i], start_date='19920101',
+                    end_date=date.today().strftime('%Y%m%d'))
+            df.to_excel("backup/daily_%s.xls" % str(stock_pool[i]))
+            print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' +
+                    str(stock_pool[i]) + ' has been backup to excel!')
             msg += str(df) + '\n'
-            # print(msg)
-            # cfg.send_mail(config, msg)
+#            print(msg)
+#            cfg.send_mail(config, msg)
         except Exception as err:
             print(err)
+
+
+def main(path):
+    config = os.path.join(path, '../ts_config.ini')
+#    print(config)
+    ts_info = cfg.read_ini(config, 'tushare')
+    ts_token = str(ts_info.get('cpp_token', None))
+#    print(ts_token)
+    pro = ts.pro_api(ts_token)
+#    print(pro)
+    stocklist = basic_data.get_stocklist(ts_token)
+    total = len(stocklist)
+    msg = ''
+    for i in range(len(stocklist)):
+        try:
+            df = pro.daily(ts_code=stocklist[i], start_date='19920101',
+                    end_date=date.today().strftime('%Y%m%d'))
+            df.to_excel("backup/daily_%s.xls" % str(stocklist[i])[:-3])
+            print('Seq: ' + str(i+1) + ' of ' + str(total) + ' Code: ' +
+                    str(stocklist[i]) + ' has been backup to excel!')
+            msg += str(df) + '\n'
+#            print(msg)
+#            cfg.send_mail(config, msg)
+        except Exception as err:
+            print(err)
+
+'''
+获取stock daily data, backup to excel sheet
+'''
+if __name__ == "__main__":
+    y_m_d = date.today().strftime('%Y%m%d')
+#    print(y_m_d)
+    cur_path = sys.argv[0].rstrip('/daily_data.py')
+#    print(cur_path)
+#    main_stockpool(cur_path)
+    main(cur_path)
