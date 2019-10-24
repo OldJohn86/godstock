5efdcb9962e5b3cd9961dab8e07dc46c69368e0a
Johnny Chen <johnchen5372@gmail.com>
2019-10-24 14:47:19 +0800

    update basic_data.py

--- demo/basic_data.py
+++ demo/basic_data.py
@@ -16,61 +16,74 @@ from datetime import date
 import tushare as ts
 import cfg
 
-exchangelist = ['SSE', 'SZSE']
-print(exchangelist)
 '''
 SSE  - 上交所
 SZSE - 深交所
 HKEX - 港交所（未上线）
 '''
+exchangelist = ['SSE', 'SZSE']
+print(exchangelist)
 
+'''
+get exhcange cal
+'''
+exchangecal = []
 def get_cal(token):
     y_m_d = date.today().strftime('%Y%m%d')
-    # print(y_m_d)
+#    print(y_m_d)
     pro = ts.pro_api(token)
     for exchange in exchangelist:
         try:
             cal = pro.trade_cal(exchange=exchange, start_date='20190101', end_date='20191231')
-            # print(cal)
+#            print(cal)
             for row in cal.itertuples():
                 if row.is_open is 1:
-                    # print(row.cal_date)
+#                    print(row.cal_date)
                     exchangecal.append(str(row.cal_date))
-            # print(exchangecal)
+#            print(exchangecal)
             cal.to_excel("backup/%s_%scal.xls" % (exchange, y_m_d))
         except Exception as err:
             print(err)
     return exchangecal
 
+'''
+get stocklist
+'''
+stocklist = []
 def get_stocklist(token):
     y_m_d = date.today().strftime('%Y%m%d')
-    # print(y_m_d)
+#    print(y_m_d)
     pro = ts.pro_api(token)
     for exchange in exchangelist:
         try:
             stock = pro.stock_basic(exchange=exchange, list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-            # print(stock)
+#            print(stock)
             for row in stock.itertuples():
-                # print(row.ts_code)
+#                print(row.ts_code)
                 stocklist.append(str(row.ts_code))
-            # print(stocklist)
+#            print(stocklist)
             stock.to_excel("backup/%s_%sstocklist.xls" % (exchange, y_m_d))
         except Exception as err:
             print(err)
     return stocklist
 
+'''
+get companylist
+'''
+companylist = []
 def get_companylist(token):
     y_m_d = date.today().strftime('%Y%m%d')
-    # print(y_m_d)
+#    print(y_m_d)
     pro = ts.pro_api(token)
     for exchange in exchangelist:
         try:
-            company = pro.stock_company(exchange=exchange, fields='ts_code,chairman,manager,secretary,reg_capital,setup_date,province')
-            # print(stock)
+            company = pro.stock_company(exchange=exchange,
+                    fields='ts_code,chairman,manager,secretary,reg_capital,setup_date,province')
+#            print(stock)
             for row in company.itertuples():
-                # print(row.ts_code)
+#                print(row.ts_code)
                 companylist.append(str(row.ts_code))
-            # print(companylist)
+#            print(companylist)
             company.to_excel("backup/%s_%scompanylist.xls" % (exchange, y_m_d))
         except Exception as err:
             print(err)
@@ -78,11 +91,10 @@ def get_companylist(token):
 
 def main(path):
     config = os.path.join(path, '../ts_config.ini')
-    print(config)
+#    print(config)
     ts_info = cfg.read_ini(config, 'tushare')
     ts_token = str(ts_info.get('cpp_token', None))
-    # print(ts_token)
- 
+#    print(ts_token)
     exchangecal = get_cal(ts_token)
     print(exchangecal)
     stocklist = get_stocklist(ts_token)
@@ -90,13 +102,9 @@ def main(path):
     companylist = get_companylist(ts_token)
     print(companylist)
 
-
-exchangecal = []
-stocklist = []
-companylist = []
 if __name__ == "__main__":
     y_m_d = date.today().strftime('%Y%m%d')
-    # print(y_m_d)
+#    print(y_m_d)
     cur_path = sys.argv[0].rstrip('/basic_data.py')
-    # print(current_path)
+#    print(current_path)
     main(cur_path)
